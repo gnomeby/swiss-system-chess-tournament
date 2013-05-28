833f9e1311c72922b28de0d85b812b0d4f9e7fbf
Andrey Niakhaichyk <andrey@niakhaichyk.org>
2013-05-28 17:03:52 +0300

    Remove useless import

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,9 +1,3 @@
-from django.contrib import admin
-from tournaments.models import Player, Tournament, Round, Game
-from django import forms
-from django.utils.functional import curry
-import math
-
 from modeladmins import player
 from modeladmins import tournament
 from modeladmins import round

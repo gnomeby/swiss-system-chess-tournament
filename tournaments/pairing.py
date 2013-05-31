7faaf445642cf8cadb29d900e2c2fe78efd87933
Andrey Niakhaichyk <andrey@niakhaichyk.org>
2013-05-31 19:33:22 +0300

    Class for pairing and unit tests

--- tournaments/pairing.py
+++ tournaments/pairing.py
@@ -1,14 +1,131 @@
 import math
 
-def pair_ordered_players(player_tuples, games):
+class Pairing:
+    '''Pairing procedure according to Swiss system rules'''
+    
+    def __init__(self, players, games=[], next_round=1):
+        ''' players - list of dicts(name, rating, title)
+        games - list of dicts(round, player(name), opponent(name||None), player_score, opponent_score, player_color, opponent_color, is_walkover)
+        round_number - number of next round
+        '''
+        self.pairs = []
+        self.players = players
+        self.games = games
+        self.next_round = next_round
+
+        # Collect player info
+        initial_info = {'score': 0, 'floats': '', 'colors': ''}
+        player_info = {player['name']:initial_info.copy() for player in players}
+        for player in players:        
+            info = player_info[player['name']]
+            info['rating'] = player['rating']
+            info['title'] = player['title']
+            info['name'] = player['name']
+        
+        # Apply game results
+        sorted_games = sorted(self.games, key=lambda game: game['name'])         
+        for game in sorted_games:
+            info = player_info[game['player']]
+            info['score'] += game['player_score']
+            #info['upfloats'] = game['player_score']
+            # B.5, B.6
+            info['floats'] += 'D' if game['player_score'] > 0 and game['is_walkover'] else '-'
+            info['colors'] += 'W' if game['player_color'] == 'W' and game['is_walkover'] else 'B'
+            
+            if game['opponent'] is None:
+                continue
+            # B.1.a
+            info['opponents'].append(game['opponent'])
+            
+            info = player_info[game['opponent']]
+            info['score'] += game['opponent_score']
+            info['floats'] += 'D' if game['opponent_score'] > 0 and game['is_walkover'] else '-'
+            info['colors'] += 'B' if game['player_color'] == 'W' and game['is_walkover'] else 'W'
+            
+            info['opponents'].append(game['player'])
+            
+        self.player_info = player_info
+        
+        # A.3
+        brackets = {}
+        for player, info in player_info:
+            if info['score'] not in brackets:
+                brackets[info['score']] = []
+            brackets[info['score']].append(player)
+        self.brackets = brackets
+
+        
+    def make_it(self):
+        if self.next_round == 1:
+            self.pair_first_round()
+            
+        return self.pairs
+    
+    def pair_first_round(self):
+        sorted_players = sorted(self.players, key=lambda player: player['name'])
+        sorted_players = sorted(sorted_players, reverse=True, key=lambda player: player['title'])
+        sorted_players = sorted(sorted_players, reverse=True, key=lambda player: player['rating'])
+        S1 = len(self.players) / 2
+        for index in range(S1):
+            self.pairs.append([sorted_players[index], sorted_players[S1+index]])
+        if len(self.players) % 2 == 1:
+            self.pairs.append([sorted_players[S1*2+1], None])
+        pass
+
+# Implemented rules: B.1, B.5, B.6
+def pair_ordered_players(player_scores, ordered_games, round_number):
     S1 = [] 
     S2 = []
 
+    players = []
+    player_info = {}
+    for score_row in player_scores:
+        players.append(score_row.player)
+        player_info[score_row.player.id] = {'score': score_row.score,
+                                            'upfloats': 0,      # B.6
+                                            'downfloats': 0,    # B.6
+                                            'prev_round_float': 0,   # B.5
+                                            'floats': ''
+                                            }
+        
+    for player in players:        
+        info = player_info[player.id]
+        info['rating'] = player.rating
+        info['title'] = int(player.fide_title[0])
+        info['name'] = player.name
+        info['color_pref'] = get_color_preference(player, ordered_games)
+        info['possible_opponents'] = get_possible_opponents(player, players, ordered_games) # B.1.a
+                
+    for game in ordered_games:
+        # B.1.b
+        if game.status == 'walkover':
+            player_id = game.player.id if game.player_score > 0.0 else game.opponent.id
+            player_info[player_id]['downfloats'] += 1            # B.1.b, B.6
+            player_info[player_id]['is_bye_allowed'] = False     # B.1.b
+            player_info[player_id]['prev_round_float'] = 'down'  # B.5
+        else:
+            player_info[game.player.id]['prev_round_float'] = None
+            if game.opponent:
+                player_info[game.opponent.id]['prev_round_float'] = None
+            pass            
+            
+    # A.3
+    brackets = {}
+    for player_id, info in player_info:
+        if info['score'] not in brackets:
+            brackets[info['score']] = []
+        brackets[info['score']].append(player_id)
+
+    # C
+    for score in sorted(brackets, reversed=True):
+        # C.1
+        problems = find_players_without_opponents(brackets[score])
+        pass                 
     current_group = []
     prev_score = None
-    for (player, score) in player_tuples:
-        if len(current_group) < 2 or prev_score == score:
-            current_group.append(player)
+    for score_row in player_scores:
+        if len(current_group) < 2 or prev_score == score_row.score:
+            current_group.append(score_row.player)
         else:
             # Pairing
             if len(current_group) % 2 == 0:
@@ -18,13 +135,11 @@ def pair_ordered_players(player_tuples, games):
                 pair_players_into_S_arrays(current_group[:-1], S1, S2)
                 current_group = current_group[-1:]
                 
-            current_group.append(player)
-        prev_score = score
+            current_group.append(score_row.player)
+        prev_score = score_row.score
     if len(current_group) > 0:
         pair_players_into_S_arrays(current_group, S1, S2)
     
-    print S1
-    print S2
     return S1 + S2
 
 def pair_players_into_S_arrays(players, S1, S2):
@@ -35,4 +150,34 @@ def pair_players_into_S_arrays(players, S1, S2):
         S1.append(player)
         S2.append(opponent)
         i+=1
+    pass
+
+def get_color_preference(player, games):
+    preference = 0
+    for game in games:
+        if game.player == player and game.status == 'finished':
+            preference += 1 if game.player_color == "W" else -1
+        if game.opponent == player and game.status == 'finished':
+            preference += 1 if game.opponent_color == "W" else -1
+            
+    return preference
+
+# B - Pairing Criteria
+# Absolute Criterias
+# B.1.a 
+def get_possible_opponents(player, players, games):
+    opponents = players[:]
+    opponents.remove(player)
+    for game in games:
+        if game.player == player and game.opponent is not None:
+            opponents.remove(game.opponent)
+        if game.opponent == player:
+            opponents.remove(game.player)
+                
+    return opponents
+
+# C.1
+def find_players_without_opponents(bracket):
+    for player_id in bracket:
+        pass
     pass
--- tournaments/tests.py
+++ tournaments/tests.py
@@ -6,11 +6,77 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from tournaments.pairing import Pairing
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class PairingTest(TestCase):
+    def __init__(self, methodName='runTest'):
+        TestCase.__init__(self, methodName=methodName)
+        self.players = [
+                        {'name': 'Alexandra Kosteniuk', 'rating': 2457, 'title': 8},
+                        {'name': 'Alisa Galliamova', 'rating': 2484, 'title': 7},
+                        {'name': 'Anna Muzychuk', 'rating': 2598, 'title': 8},
+                        {'name': 'Antoaneta Stefanova', 'rating': 2518, 'title': 8},
+                        {'name': 'Betul Cemre Yildiz', 'rating': 2333, 'title': 6},
+                        {'name': 'Elina Danielian', 'rating': 2484, 'title': 8},
+                        {'name': 'Hou Yifan', 'rating': 2623, 'title': 8},
+                        {'name': 'Humpy Koneru', 'rating': 2589, 'title': 8},
+                        {'name': 'Kateryna Lahno', 'rating': 2546, 'title': 8},
+                        {'name': 'Nadezhda Kosintseva', 'rating': 2528, 'title': 8},
+                        {'name': 'Tatiana Kosintseva', 'rating': 2532, 'title': 8},
+                        {'name': 'Viktorija Cmilyte', 'rating': 2508, 'title': 8},
+                       ]
+
+    def find_player(self, name, list=None):
+        if list is None:
+            list = self.players
+        for player in list:
+            if player['name'] == name:
+                return player
+            
+        return None
+
+    def test_pairing_for_first_round(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests ordering by rating, title, name and simple pairing.
         """
-        self.assertEqual(1 + 1, 2)
+        
+        # Test name ordering
+        players = [
+                    {'name': 'Ad', 'rating': 0, 'title': 0},
+                    {'name': 'Aa', 'rating': 0, 'title': 0},
+                    {'name': 'Ab', 'rating': 0, 'title': 0},
+                    {'name': 'Ac', 'rating': 0, 'title': 0},
+                   ]
+        pairing = Pairing(players)
+        pairs = pairing.make_it()
+        
+        self.assertIn(self.find_player('Aa', players), pairs[0])
+        self.assertIn(self.find_player('Ac', players), pairs[0])
+
+        self.assertIn(self.find_player('Ab', players), pairs[1])
+        self.assertIn(self.find_player('Ad', players), pairs[1])
+
+        
+        # Test rating and title ordering
+        pairing = Pairing(self.players)
+        pairs = pairing.make_it()
+        
+        self.assertIn(self.find_player('Hou Yifan'), pairs[0])
+        self.assertIn(self.find_player('Antoaneta Stefanova'), pairs[0])
+
+        self.assertIn(self.find_player('Anna Muzychuk'), pairs[1])
+        self.assertIn(self.find_player('Viktorija Cmilyte'), pairs[1])
+
+        self.assertIn(self.find_player('Humpy Koneru'), pairs[2])
+        self.assertIn(self.find_player('Elina Danielian'), pairs[2])
+        
+        self.assertIn(self.find_player('Kateryna Lahno'), pairs[3])
+        self.assertIn(self.find_player('Alisa Galliamova'), pairs[3])
+        
+        self.assertIn(self.find_player('Tatiana Kosintseva'), pairs[4])
+        self.assertIn(self.find_player('Alexandra Kosteniuk'), pairs[4])
+
+        self.assertIn(self.find_player('Nadezhda Kosintseva'), pairs[5])
+        self.assertIn(self.find_player('Betul Cemre Yildiz'), pairs[5])
+        
